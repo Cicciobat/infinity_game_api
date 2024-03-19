19649ce12b03fa8b140936d433a0afb7f9a79c8b
Francesco Maria Battaglia <francescomariabattaglia@gmail.com>
2024-03-19 11:50:29 +0100

    feat(token): add token verification to access the api

--- app.js
+++ app.js
@@ -2,14 +2,50 @@ const express = require('express')
 const path = require("path");
 const app = express()
 const jwt = require('jsonwebtoken')
+const cors = require("cors");
+const bodyParser = require("body-parser");
 require('dotenv').config()
 const PORT = process.env.PORT
+const SECRET_KEY = process.env.SECRET_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) => (
+const verifyToken = (req, res, next) => {
+    const token = req.header('Authorization');
+    console.log(token.toString())
+
+    try {
+        req.user = jwt.verify(token, SECRET_KEY);
+        next();
+    } catch (error) {
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+}
+
+const generateToken = (req, res, next) => {
+    try {
+        if (req.body.key && req.body.key === PRIVATE_KEY) {
+            let key = req.body.key;
+            req.token =  jwt.sign({'key': key}, SECRET_KEY, {
+                expiresIn: '7 days',
+                algorithm: 'HS256'
+            })
+            next()
+        } else  {
+            return res.status(401).send('Login denied - Unauthorized')
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+app.get('/',  (req, res) => (
     res.status(200).sendFile(path.join(__dirname, '/web/index.html'), {
         headers: {
             'Content-Type': 'text/html; charset=utf-8',
@@ -19,8 +55,8 @@ app.get('/', (req, res) => (
     })
 ))
 
-app.get('/players', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '/assets/players.json'), {
+app.get('/players', verifyToken,  (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, '/assets/players.json'), {
         headers: {
             'Content-Type': 'application/json',
             'x-timestamp': Date.now(),
@@ -29,8 +65,8 @@ app.get('/players', (req, res) => {
     })
 })
 
-app.get('/teams', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '/assets/teams.json'), {
+app.get('/teams',verifyToken, (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, '/assets/teams.json'), {
         headers: {
             'Content-Type': 'application/json',
             'x-timestamp': Date.now(),
@@ -39,8 +75,17 @@ app.get('/teams', (req, res) => {
     })
 })
 
+app.post('/auth', generateToken,  (req, res) => {
+    res.headers = {
+        'Content-Type': 'application/json',
+        'x-timestamp': Date.now(),
+        'Age': 0
+    }
+    return res.status(200).json(req.token)
+})
 
-app.use((req, res, next) => {
+
+app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, '/web/404.html'), {
         headers: {
             'Content-Type': 'text/html; charset=utf-8',
